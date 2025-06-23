b7e1e010f2cec7977900aeb3183fb3f0b0c0e749
YassineHamdouni <yassinehamdouni023@gmail.com>
2025-06-23 18:11:54 +0100

    adding the mkdir

--- shell/target/debug/deps/log-a2811925b77a9eb7.d
+++
@@ -1,8 +0,0 @@
-/home/yhamdoun/0-shell/shell/target/debug/deps/liblog-a2811925b77a9eb7.rmeta: /home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/lib.rs /home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/macros.rs /home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/serde.rs /home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/__private_api.rs
-
-/home/yhamdoun/0-shell/shell/target/debug/deps/log-a2811925b77a9eb7.d: /home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/lib.rs /home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/macros.rs /home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/serde.rs /home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/__private_api.rs
-
-/home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/lib.rs:
-/home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/macros.rs:
-/home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/serde.rs:
-/home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.27/src/__private_api.rs:
--- shell/target/debug/deps/shell-8b40ca0aa86678a4.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/soufi/0-shell/shell/target/debug/deps/shell-8b40ca0aa86678a4: src/main.rs
-
-/mnt/c/Users/soufi/0-shell/shell/target/debug/deps/shell-8b40ca0aa86678a4.d: src/main.rs
-
-src/main.rs:
