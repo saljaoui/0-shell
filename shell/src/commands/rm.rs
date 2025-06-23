afad9387ac94069c2a46a08000275945fa53471d
saljaoui <soufian2022ff@gamil.com>
2025-06-23 18:34:12 +0100

    update

--- shell/src/commands/rm.rs
+++ shell/src/commands/rm.rs
@@ -4,6 +4,17 @@ use std::fs;
 
 pub fn builtin_rm(args: &[&str]) {
     println!("{:?}", args);
+    if args.contains(&"-r") {
+    let r = fs::remove_dir(args[0]);
+    println!("{:?}", r);
+    println!("dir");
+    } else {
     let r = fs::remove_file(args[0]);
     println!("{:?}", r);
+    println!("file");
+    }
+    // let r = fs::remove_file(args[0]);
+    // println!("{:?}", r);
+        
+
 }
