a57337e76be644e3be21f047758bcef7e9fd83f5
saljaoui <soufian2022ff@gamil.com>
2025-06-23 18:22:33 +0100

    update

--- shell/src/commands/rm.rs
+++ shell/src/commands/rm.rs
@@ -1,3 +1,9 @@
+// use std::fs::remove_file;
+use std::fs;
+
+
 pub fn builtin_rm(args: &[&str]) {
     println!("{:?}", args);
+    let r = fs::remove_file(args[0]);
+    println!("{:?}", r);
 }
---
+++ shell/target/debug/deps/shell-05d58a0876cde5e5.d
@@ -0,0 +1,10 @@
+/home/saljaoui/0-shell/shell/target/debug/deps/shell-05d58a0876cde5e5: src/main.rs src/dispatch.rs src/commands/mod.rs src/commands/ls.rs src/commands/echo.rs src/commands/rm.rs
+
+/home/saljaoui/0-shell/shell/target/debug/deps/shell-05d58a0876cde5e5.d: src/main.rs src/dispatch.rs src/commands/mod.rs src/commands/ls.rs src/commands/echo.rs src/commands/rm.rs
+
+src/main.rs:
+src/dispatch.rs:
+src/commands/mod.rs:
+src/commands/ls.rs:
+src/commands/echo.rs:
+src/commands/rm.rs:
