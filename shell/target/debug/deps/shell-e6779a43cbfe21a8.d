8e78a7755e24f076cbf9fd608384bbcdb64b8c43
yakhaldy <you@example.com>
2025-06-24 15:18:16 +0100

    add ls

---
+++ shell/target/debug/deps/shell-e6779a43cbfe21a8.d
@@ -0,0 +1,12 @@
+/home/yakhaldy/0-shell/shell/target/debug/deps/shell-e6779a43cbfe21a8: src/main.rs src/dispatch.rs src/commands/mod.rs src/commands/ls.rs src/commands/echo.rs src/commands/cat.rs src/commands/mkdir.rs src/commands/cd.rs
+
+/home/yakhaldy/0-shell/shell/target/debug/deps/shell-e6779a43cbfe21a8.d: src/main.rs src/dispatch.rs src/commands/mod.rs src/commands/ls.rs src/commands/echo.rs src/commands/cat.rs src/commands/mkdir.rs src/commands/cd.rs
+
+src/main.rs:
+src/dispatch.rs:
+src/commands/mod.rs:
+src/commands/ls.rs:
+src/commands/echo.rs:
+src/commands/cat.rs:
+src/commands/mkdir.rs:
+src/commands/cd.rs:
