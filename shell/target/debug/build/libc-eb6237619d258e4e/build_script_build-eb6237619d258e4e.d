d557319d42f858c9d64559674b59a7ca0236e074
YassineHamdouni <yassinehamdouni023@gmail.com>
2025-06-25 11:17:37 +0100

    handling warnings

--- shell/target/debug/build/libc-eb6237619d258e4e/build_script_build-eb6237619d258e4e.d
+++
@@ -1,5 +0,0 @@
-/home/yhamdoun/0-shell/shell/target/debug/build/libc-eb6237619d258e4e/build_script_build-eb6237619d258e4e: /home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.174/build.rs
-
-/home/yhamdoun/0-shell/shell/target/debug/build/libc-eb6237619d258e4e/build_script_build-eb6237619d258e4e.d: /home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.174/build.rs
-
-/home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.174/build.rs:
--- shell/target/debug/build/num-traits-ab3219886afc9143/build_script_build-ab3219886afc9143.d
+++
@@ -1,5 +0,0 @@
-/home/yhamdoun/0-shell/shell/target/debug/build/num-traits-ab3219886afc9143/build_script_build-ab3219886afc9143: /home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs
-
-/home/yhamdoun/0-shell/shell/target/debug/build/num-traits-ab3219886afc9143/build_script_build-ab3219886afc9143.d: /home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs
-
-/home/yhamdoun/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs:
