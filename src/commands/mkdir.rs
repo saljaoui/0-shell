29113922d8c24a2d965d941a53cfc5112443cc81
YassineHamdouni <yassinehamdouni023@gmail.com>
2025-07-18 11:38:21 +0100

    fix mkdir

--- src/commands/mkdir.rs
+++ src/commands/mkdir.rs
@@ -3,12 +3,16 @@ use std::path::Path;
 
 pub fn builtin_mkdir(args: &[&str]) {
     for arg in args {
+        if arg.starts_with("-") {
+            println!("mkdir: cannot create this folder: {}, because it starts with '-'", arg);
+            continue;
+        }
         let path = Path::new(arg);
         if path.exists() {
             println!("mkdir: cannot create directory '{}': File exists", arg);
         } else {
             match fs::create_dir(path) {
-                Ok(_) => {},
+                Ok(_) => {}
                 Err(e) => {
                     let error = e.to_string();
                     let error_clean = error.split(" (os error").next().unwrap_or(&error);
